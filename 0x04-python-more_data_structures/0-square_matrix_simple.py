ef4503f93f9d6c615d80440442451e57803110c7
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-09-28 12:50:36 +0100

    Data stuctures: sets

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_list = []
+    for i in range(0, len(matrix)):
+        new_list += [list(map(lambda x: x ** 2, matrix[i]))]
+    return new_list
