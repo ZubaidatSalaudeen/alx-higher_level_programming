d66f5266787bf9ee1b1d344bcc1a64db05166c71
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-10-11 17:50:30 +0100

    OOP

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+
+This module contains a class that defines a square.
+
+"""
+
+
 class Square():
-    """
-    Defines a square
-    """
+    """Defines a square."""
+
     pass
