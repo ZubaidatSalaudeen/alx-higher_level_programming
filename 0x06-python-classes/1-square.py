e358e780cec49e07ff09e0573be66116f337c1e2
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-10-11 12:13:18 +0100

    OOP

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Defines a square
+    """
+    def __init__(self, size):
+        """
+        Instantiates the suare with size
+        """
+        self.__size = size
